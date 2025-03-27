53d56d2655777fb3aa46b1b5dae8ae48f80a75b1
Kimgyuilli <rlarbdlf222@naver.com>
2025-03-27 23:30:07 +0900

    5강까지 완료

---
+++ member/src/main/java/com/example/member/domain/MemberEntity.java
@@ -0,0 +1,24 @@
+package com.example.member.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String memberEmail;
+
+    @Column
+    private String memberName;
+
+    @Column
+    private String memberPassword;
+}
--- member/src/main/java/com/example/member/repository/MemberRepository.java
+++ member/src/main/java/com/example/member/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.example.member.repository;
 
+import com.example.member.domain.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 }
