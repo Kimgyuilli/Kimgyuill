880d739da1d893e8b4d5359d26e343192fb6dff3
Kimgyuilli <rlarbdlf222@naver.com>
2025-03-27 20:51:28 +0900

    5장 db까지 연결

---
+++ member/src/main/java/com/example/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.example.member.controller;
+
+import com.example.member.dto.MemberDTO;
+import com.example.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("이메일: " + memberDTO.getMemberEmail());
+        System.out.println("비밀번호: " + memberDTO.getMemberPassword());
+        System.out.println("이름: " + memberDTO.getMemberName());
+
+        memberService.save(memberDTO);
+
+        return "index";
+    }
+}
